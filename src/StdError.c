f0210059cd9e8d9f8332e971582a7f240169a365
Braxton Salyer <braxtonsalyer@gmail.com>
2021-08-15 01:12:36 -0500

    Add file-level docs and License tags to implementation files (.c files)

--- src/StdError.c
+++ src/StdError.c
@@ -1,3 +1,31 @@
+/// @file StdError.h
+/// @author Braxton Salyer <braxtonsalyer@gmail.com>
+/// @brief This module provides an extensible type for communicating errors via both error codes and
+/// message strings.
+/// @version 0.1
+/// @date 2021-08-15
+///
+/// MIT License
+/// @copyright Copyright (c) 2021 Braxton Salyer <braxtonsalyer@gmail.com>
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in all
+/// copies or substantial portions of the Software.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+/// SOFTWARE.
+
 #if defined(_MSC_VER) || defined(_WIN32) || defined(_WIN64)
 	// NOLINTNEXTLINE
 	#define _CRT_SECURE_NO_WARNINGS
