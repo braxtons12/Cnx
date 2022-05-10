2468a8466d2fcde3bf646ec6bf8312de2d97faeb
braxtons12 <braxtonsalyer@gmail.com>
2022-05-10 00:42:10 -0500

    Significant improvements to timepoint formatting performance

--- src/time/TimePoint.c
+++ src/time/TimePoint.c
@@ -29,6 +29,8 @@
 #include <Cnx/time/TimePoint.h>
 #include <time.h>
 
+#define unlikely(x) __builtin_expect(!!(x), 0)
+
 CnxTimePoint cnx_time_point_new(CnxDuration time_since_epoch) {
 	return cnx_time_point_new_with_clock(time_since_epoch, &cnx_system_clock);
 }
@@ -117,13 +119,13 @@ CnxResult(tm) cnx_time_point_as_tm(CnxTimePoint to_cast) {
 #if _POSIX_C_SOURCE >= 1 || _XOPEN_SOURCE || _BSD_SOURCE || _SVID_SOURCE || _POSIX_SOURCE
 		tm as_tm;
 		let res = localtime_r(&time, &as_tm);
-		if(res == nullptr) {
+		if(unlikely(res == nullptr)) {
 			return Err(tm, cnx_error_new(errno, CNX_POSIX_ERROR_CATEGORY));
 		}
 		return Ok(tm, as_tm);
 #else
 		let res = localtime(&time);
-		if(res == nullptr) {
+		if(unlikely(res == nullptr)) {
 			return Err(tm, cnx_error_new(errno, CNX_POSIX_ERROR_CATEGORY));
 		}
 		return Ok(tm, *res);
@@ -134,13 +136,13 @@ CnxResult(tm) cnx_time_point_as_tm(CnxTimePoint to_cast) {
 #if _POSIX_C_SOURCE >= 1 || _XOPEN_SOURCE || _BSD_SOURCE || _SVID_SOURCE || _POSIX_SOURCE
 		tm as_tm;
 		let res = gmtime_r(&time, &as_tm);
-		if(res == nullptr) {
+		if(unlikely(res == nullptr)) {
 			return Err(tm, cnx_error_new(errno, CNX_POSIX_ERROR_CATEGORY));
 		}
 		return Ok(tm, as_tm);
 #else
 		let res = gmtime(&time);
-		if(res == nullptr) {
+		if(unlikely(res == nullptr)) {
 			return Err(tm, cnx_error_new(errno, CNX_POSIX_ERROR_CATEGORY));
 		}
 		return Ok(tm, *res);
@@ -222,58 +224,161 @@ CnxCompare cnx_time_point_compare(CnxTimePoint lhs, CnxTimePoint rhs) {
 	return cnx_duration_compare(lhs.time_since_epoch, rhs.time_since_epoch);
 }
 
+static const char digits[100][2] = {
+	{'0', '0'},
+	{'0', '1'},
+	{'0', '2'},
+	{'0', '3'},
+	{'0', '4'},
+	{'0', '5'},
+	{'0', '6'},
+	{'0', '7'},
+	{'0', '8'},
+	{'0', '9'},
+	{'1', '0'},
+	{'1', '1'},
+	{'1', '2'},
+	{'1', '3'},
+	{'1', '4'},
+	{'1', '5'},
+	{'1', '6'},
+	{'1', '7'},
+	{'1', '8'},
+	{'1', '9'},
+	{'2', '0'},
+	{'2', '1'},
+	{'2', '2'},
+	{'2', '3'},
+	{'2', '4'},
+	{'2', '5'},
+	{'2', '6'},
+	{'2', '7'},
+	{'2', '8'},
+	{'2', '9'},
+	{'3', '0'},
+	{'3', '1'},
+	{'3', '2'},
+	{'3', '3'},
+	{'3', '4'},
+	{'3', '5'},
+	{'3', '6'},
+	{'3', '7'},
+	{'3', '8'},
+	{'3', '9'},
+	{'4', '0'},
+	{'4', '1'},
+	{'4', '2'},
+	{'4', '3'},
+	{'4', '4'},
+	{'4', '5'},
+	{'4', '6'},
+	{'4', '7'},
+	{'4', '8'},
+	{'4', '9'},
+	{'5', '0'},
+	{'5', '1'},
+	{'5', '2'},
+	{'5', '3'},
+	{'5', '4'},
+	{'5', '5'},
+	{'5', '6'},
+	{'5', '7'},
+	{'5', '8'},
+	{'5', '9'},
+	{'6', '0'},
+	{'6', '1'},
+	{'6', '2'},
+	{'6', '3'},
+	{'6', '4'},
+	{'6', '5'},
+	{'6', '6'},
+	{'6', '7'},
+	{'6', '8'},
+	{'6', '9'},
+	{'7', '0'},
+	{'7', '1'},
+	{'7', '2'},
+	{'7', '3'},
+	{'7', '4'},
+	{'7', '5'},
+	{'7', '6'},
+	{'7', '7'},
+	{'7', '8'},
+	{'7', '9'},
+	{'8', '0'},
+	{'8', '1'},
+	{'8', '2'},
+	{'8', '3'},
+	{'8', '4'},
+	{'8', '5'},
+	{'8', '6'},
+	{'8', '7'},
+	{'8', '8'},
+	{'8', '9'},
+	{'9', '0'},
+	{'9', '1'},
+	{'9', '2'},
+	{'9', '3'},
+	{'9', '4'},
+	{'9', '5'},
+	{'9', '6'},
+	{'9', '7'},
+	{'9', '8'},
+	{'9', '9'}
+};
+
+__attr(always_inline) __attr(nodiscard) static inline char timepoint_parse_int(int num, int digit) {
+	return static_cast(char)(digit < 2 ? digits[num % 100][digit] // NOLINT
+										 :
+										 digits[num / 100][digit - 2] // NOLINT
+	);
+}
+
 CnxString cnx_time_point_human_readable_format(CnxTimePoint self, CnxAllocator allocator) {
+	if(unlikely(self.locale == CNX_LOCAL_TIME)) {
+		self = cnx_convert_local_time_to_utc(self);
+	}
+
 	let_mut maybe_parsed = cnx_time_point_as_tm(self);
-	let parsed_lvalue
-		= cnx_result_expect(maybe_parsed, "Failed to convert CnxTimePoint to struct tm");
-	let parsed = &parsed_lvalue;
+	let parsed = cnx_result_unwrap(maybe_parsed);
+	let year = parsed.tm_year + 1900; // NOLINT
+	let month = parsed.tm_mon + 1;
 
-	// NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers, readability-magic-numbers)
-	char memory[20] = {0};
-
-	if(self.locale != CNX_UNKNOWN_TIME) {
-		// NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers, readability-magic-numbers)
-		char utc[6] = {0};
-
-		// NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers, readability-magic-numbers)
-		__attr(maybe_unused) usize utc_written = strftime(utc, 6, "%z", parsed);
-		cnx_assert(utc_written == 5, "Failed to format time point");
-		CnxScopedString utc_string
-			// NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers, readability-magic-numbers)
-			= cnx_string_new_with_capacity_with_allocator(6, allocator);
-		let hours = cnx_stringview_from(utc, 0, 3);
-		let minutes = cnx_stringview_from(utc, 3, 2);
-		cnx_string_push_back(utc_string, ' ');
-		cnx_string_append(utc_string, &hours);
-		cnx_string_append(utc_string, &minutes);
-
-#if CNX_PLATFORM_COMPILER_CLANG
-		// NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers, readability-magic-numbers)
-		__attr(maybe_unused) let written = strftime(memory, 20, "%F|%T", parsed);
-#else
-		// NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers, readability-magic-numbers)
-		__attr(maybe_unused) let written = strftime(memory, 20, "%Y-%m-%d|%H:%M:%S", parsed);
-#endif // CNX_PLATFORM_COMPILER_CLANG
-		cnx_assert(written == 19, "Failed to format time point");
-		CnxScopedString str = cnx_string_from_with_allocator(memory, allocator);
-		cnx_string_append(str, &utc_string);
-		return move(str);
-	}
-	else {
-#if CNX_PLATFORM_COMPILER_CLANG
-		// NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers, readability-magic-numbers)
-		__attr(maybe_unused) let written = strftime(memory, 20, "%F|%T", parsed);
-#else
-		// NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers, readability-magic-numbers)
-		__attr(maybe_unused) let written = strftime(memory, 20, "%Y-%m-%d|%H:%M:%S", parsed);
-#endif // CNX_PLATFORM_COMPILER_CLANG
-		cnx_assert(written == 19, "Failed to format time point");
-		return cnx_string_from_with_allocator(memory, allocator);
-	}
+	//// NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers, readability-magic-numbers)
+	const char memory[20] = {
+		[0] = timepoint_parse_int(year, 2),
+		[1] = timepoint_parse_int(year, 3),
+		[2] = timepoint_parse_int(year, 0),
+		[3] = timepoint_parse_int(year, 1),
+		[4] = '-',
+		[5] = timepoint_parse_int(month, 0),		   // NOLINT
+		[6] = timepoint_parse_int(month, 1),		   // NOLINT
+		[7] = '-',									   // NOLINT
+		[8] = timepoint_parse_int(parsed.tm_mday, 0),  // NOLINT
+		[9] = timepoint_parse_int(parsed.tm_mday, 1),  // NOLINT
+		[10] = '|',									   // NOLINT
+		[11] = timepoint_parse_int(parsed.tm_hour, 0), // NOLINT
+		[12] = timepoint_parse_int(parsed.tm_hour, 1), // NOLINT
+		[13] = ':',									   // NOLINT
+		[14] = timepoint_parse_int(parsed.tm_min, 0),  // NOLINT
+		[15] = timepoint_parse_int(parsed.tm_min, 1),  // NOLINT
+		[16] = ':',									   // NOLINT
+		[17] = timepoint_parse_int(parsed.tm_sec, 0),  // NOLINT
+		[18] = timepoint_parse_int(parsed.tm_sec,
+								   1), // NOLINT
+									   //[19] = ' ',									   // NOLINT
+									   //[20] = '+',									   // NOLINT
+									   //[21] = '0',									   // NOLINT
+									   //[22] = '0',									   // NOLINT
+									   //[23] = '0',									   // NOLINT
+									   //[24] = '0',									   // NOLINT
+	};
+	//// NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers, readability-magic-numbers)
+	return cnx_string_from_cstring_with_allocator(memory, 20, allocator);
 }
 
 CnxString cnx_time_point_format(const CnxFormat* restrict self, CnxFormatSpecifier specifier) {
-	return cnx_duration_format_with_allocator(self, specifier, DEFAULT_ALLOCATOR);
+	return cnx_time_point_format_with_allocator(self, specifier, DEFAULT_ALLOCATOR);
 }
 
 CnxString cnx_time_point_format_with_allocator(const CnxFormat* restrict self,
